4501bf3ae3ca1850b020fa391c9e7ef894ca122c
Josh Jangula <75451637+QualityOwl@users.noreply.github.com>
2023-03-23 16:15:21 -0500

    Add 'WebDriverDemo.Extensions' project.

---
+++ WebDriverDemo.Extensions/Logging/XunitExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Xunit.Abstractions;
+
+namespace WebDriverDemo.Extensions.Logging
+{
+    public static class XunitExtensions
+    {
+        private static int _stepNumber = 1;
+
+        public static void StepDescription(this ITestOutputHelper output, string input)
+        {
+            output.WriteLine($"Step #{(_stepNumber <= 9 ? $"{_stepNumber} " : _stepNumber)} *** {input}");
+
+            _stepNumber++;
+        }
+
+        public static void HeaderText(this ITestOutputHelper output, string input = "Test started!")
+        {
+            var logText = $"======== {input} ========";
+
+            output.WriteLine(logText);
+            
+            output.WriteLine(new string('=', logText.Length));
+        }
+
+        public static void FooterText(this ITestOutputHelper output, string input = "Test completed!")
+        {
+            var logText = $"======== {input} ========";
+
+            output.WriteLine(new string('=', logText.Length));
+
+            output.WriteLine(logText);
+        }
+    }
+}
---
+++ WebDriverDemo.Extensions/WebDriverExtensions.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebDriverDemo.Extensions
+{
+    public static class WebDriverExtensions
+    {
+        public static void WaitForPageLoad(this IWebDriver webDriver, int timeoutInSeconds = 60)
+        {
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(timeoutInSeconds));
+
+            wait.Until(d =>
+            {
+                string readyState = ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").ToString();
+
+                return readyState.Equals("complete", StringComparison.OrdinalIgnoreCase);
+            });
+        }
+    }
+}

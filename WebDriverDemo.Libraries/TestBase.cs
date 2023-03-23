fadd7236943205f8c196ad46e2ed253d1e2ac3f5
Josh Jangula <75451637+QualityOwl@users.noreply.github.com>
2023-03-23 16:15:39 -0500

    Add 'WebDriverDemo.Libraries' project.

---
+++ WebDriverDemo.Libraries/TestBase.cs
@@ -0,0 +1,27 @@
+using OpenQA.Selenium;
+using System;
+using WebDriverDemo.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace WebDriverDemo.Libraries
+{
+    public abstract class TestBase : IDisposable
+    {
+        protected ITestOutputHelper Log;
+        protected IWebDriver WebDriver;
+
+        public TestBase(ITestOutputHelper log)
+        {
+            Log = log;
+
+            Log.HeaderText();
+        }
+
+        public void Dispose()
+        {
+            WebDriver.Quit();
+
+            Log.FooterText();
+        }
+    }
+}
